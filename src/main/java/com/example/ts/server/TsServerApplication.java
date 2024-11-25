7bd1d31aa69806698d3b59b0cdab6e6f0b6e9150
Aashritha Ancel Nudurupati <aashrithaancelnudurupati@gmail.com>
2024-11-25 16:17:58 +0530

    successful updation

---
+++ src/main/java/com/example/ts/server/TsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ts.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsServerApplication.class, args);
+	}
+
+}
